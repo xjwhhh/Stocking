62cd3071345caedd42cbc94de9ccc3754b79a6d3
151250162 <151250162@smail.nju.edu.cn>
2017-05-22 23:11:04 +0800

    均值计算

---
+++ src/main/java/stocking/calculation/smaCal.py
@@ -0,0 +1,13 @@
+'''
+计算均值
+:param
+adjClose(series)，interval
+'''
+
+
+def smaCal(tsPrice, k):
+    import pandas as pd
+    sma = pd.Series(0.0, index=tsPrice.index)
+    for i in range(k - 1, len(tsPrice)):
+        sma[i] = sum(tsPrice[(i - k + 1):(i + 1)]) / k
+    return (sma)
